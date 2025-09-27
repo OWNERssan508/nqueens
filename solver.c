2c3e24de2c63b60070a740eedb85ce39b0fe7a8c
Seth <thesius@hwsrv-1298729.hostwindsdns.com>
2025-09-27 04:12:51 +0000

    All Solutions

--- solver.c
+++ solver.c
@@ -5,6 +5,7 @@ bool solve_nqueens_util(int board[], int col, int n) {
     if (col >= n) {
         print_board(board, n); // Print the current solution
         return false; 
+	// Changed True --> False
     }
 
     for (int i = 0; i < n; i++) {
